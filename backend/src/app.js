2d25d68f03fddb6b032f5b624d89e8f4190ba1c6
william <willshinji@gmail.com>
2019-02-11 16:02:50 -0200

    refactor app and server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,5 @@
 const app = require('./config/server')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended : true}))
+app.use(bodyParser.json())
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,12 +1,7 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-
 const app = express()
 
-const port = 3003
-
-app.use(bodyParser.urlencoded({ extended : true}))
-app.use(bodyParser.json())
+const port = process.env.PORT || 3003
 
 app.listen(port, () => console.log(`API is running on port ${port}`))
 
