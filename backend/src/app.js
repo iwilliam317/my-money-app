837c51b4ba229b76fdb0dd5176b3be5080515c2a
william <willshinji@gmail.com>
2019-02-11 20:54:26 -0200

    set cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,9 @@
 const app = require('./config/server')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({ extended : true}))
 app.use(bodyParser.json())
+app.use(cors())
 
 require('./config/routes')(app)
