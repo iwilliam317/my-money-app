474131c0b5bec39e106baea3ea09d6693f07c64e
william <willshinji@gmail.com>
2019-02-11 20:58:57 -0200

    set logger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,19 @@
 const app = require('./config/server')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
 
-app.use(bodyParser.urlencoded({ extended : true}))
+
+const accessLogStream = fs.createWriteStream(
+    path.join(`${__dirname}/logs`, 'access.log'), { flags: 'a' }
+)
+
 app.use(bodyParser.json())
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended : true}))
+
+app.use(morgan('combined', { stream: accessLogStream }))
 
 require('./config/routes')(app)
