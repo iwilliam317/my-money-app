787144af3d0e052a6e11aa841a858be262c1c41d
william <willshinji@gmail.com>
2019-02-13 15:22:23 -0200

    fix mongodb connection - docker

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGOLAB_URI_MY_MONEY || 'mongodb://localhost:27017/mymoney'
+const url = process.env.MONGOLAB_URI_MY_MONEY || 'mongodb://localhost:27017/mymoney' || 'mongodb://mongo:27017/mymoney'
 
 mongoose.connect(url, (error, response) => {
   if (error) console.log (`ERROR connecting to:  ${url} -  ${error}`);
   else console.log (`Succeeded connected to: ${url}`);
 })
+
+mongoose.Promise = global.Promise;
 
 //mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
