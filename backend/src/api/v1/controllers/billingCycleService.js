150a31a8cd78fb79cac909e0a74a79a3fadc31f8
william <willshinji@gmail.com>
2019-02-16 13:51:15 -0200

    create route count

--- backend/src/api/v1/controllers/billingCycleService.js
+++ backend/src/api/v1/controllers/billingCycleService.js
@@ -3,4 +3,13 @@ const BillingCycle = require('../models/billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.route('count', (req, res) => {
+    BillingCycle.count((error, value) => {
+        if (error) 
+            return res.status(500).json({ errors: [error]})
+        
+        res.json({ value })
+    })
+})
+
 module.exports = BillingCycle
