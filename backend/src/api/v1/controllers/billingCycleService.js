5a19db40e993450161772edc07be4fd63728ea2e
william <willshinji@gmail.com>
2019-02-18 13:59:46 -0300

    route summary - finished

--- backend/src/api/v1/controllers/billingCycleService.js
+++ backend/src/api/v1/controllers/billingCycleService.js
@@ -14,15 +14,31 @@ BillingCycle.route('count', (req, res, next) => {
 
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate([
-        { $project: {
+        { 
+            $project: {
             _id: 0,
             credit : { $sum : "$credits.value" },
             debit: { $sum : "$debts.value" }
-        } },
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                credit: { $sum : "$credit" },
+                debit: { $sum : "$debit" }
+            }
+        },
+        {
+            $project : {
+                _id: 0,
+                credit: 1,
+                debit: 1
+            }
+        }
     ]).exec((error, result) => {
         if(error)
             return res.status(500).json({ errors: [error]})
-        res.send({ result })
+        res.send(result[0] || { credit : 0 , debit : 0} )
     })
 })
 
