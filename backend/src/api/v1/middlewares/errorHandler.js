628e9206d1a1a9ebc781b931a9b770b3598f4702
william <willshinji@gmail.com>
2019-02-25 13:56:23 -0300

    middleware for handling errors

--- backend/src/api/v1/middlewares/errorHandler.js
+++ backend/src/api/v1/middlewares/errorHandler.js
@@ -1,5 +1,17 @@
+const _ = require('lodash')
+
 module.exports = (req, res, next) => {
-    console.log('passing through middleware in construction...')
-    console.log(res.locals.bundle)
+    
+    if(res.locals.bundle.errors){
+        const errors = parseErrors(res.locals.bundle.errors)
+        return res.status(500).send({ errors })
+    }
+       
     next()
+}
+
+const parseErrors = nodeRestfulErrors => {
+    const errors = []
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+    return errors
 }
