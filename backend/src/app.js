0c16e859915a03ef59df668f74db434cd0791088
william <willshinji@gmail.com>
2019-02-26 00:11:42 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 const morgan = require('morgan')
 const fs = require('fs')
 const path = require('path')
-const errorHandler = require('./api/v1/middlewares/errorHandler')
 
 const accessLogStream = fs.createWriteStream(
     path.join(`${__dirname}/logs`, 'access.log'), { flags: 'a' }
@@ -19,3 +18,5 @@ app.use(morgan('combined', { stream: accessLogStream }))
 //  app.use(errorHandler)
 
 require('./config/routes')(app)
+
+module.exports = app
