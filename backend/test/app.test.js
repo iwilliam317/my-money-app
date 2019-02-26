0f9d8333628160699127b09104029377c62f3e60
william <willshinji@gmail.com>
2019-02-26 20:28:50 -0300

    add app.test.js

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -6,10 +6,16 @@ chai.use(chaiHttp)
 
 const app = require('../src/app')
 const url = '/api/v1/billing-cycles'
+const BillingCycle = require('../src/api/v1/models/billingCycle')
 
 console.log(`TESTS RUNNING ON ${process.env.NODE_ENV} MODE`)
 
 describe('api', () => {
+
+    before(() => {
+        BillingCycle.remove({})
+    })
+    
     context('#GET /api/v1/billing-cycles', () => {
          it('should return status code 200', done => {
             chai.request(app)
@@ -24,7 +30,7 @@ describe('api', () => {
             chai.request(app)
                 .get('/api/v1/billing-cycles')
                 .end((err, res) => {
-                    expect(res.body.length).to.equals(4)
+                    expect(res.body.length).to.equals(0)
                     done()
                 })
         })
