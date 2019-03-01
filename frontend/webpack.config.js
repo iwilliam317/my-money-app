f66e6a67e1b2076e84d03c045df6757b317fb195
william <willshinji@gmail.com>
2019-03-01 16:41:58 -0300

    set weback config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -18,5 +18,29 @@ module.exports = {
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery' : 'jquery'
+        }), new ExtractTextPlugin('app.css')
+    ],
+    modules : {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            loader: 'file'
+        }]
     }
 }
