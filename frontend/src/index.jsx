3ac841099b85cee220202206c60f4300abf4cc6c
william <willshinji@gmail.com>
2019-03-06 20:32:22 -0300

    create app component

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>HEY</h1>, document.getElementById('app'))
+import App from './main/app'
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,12 @@
+import '../common/template/dependencies'
+import React, { Component } from 'react'
+
+class App extends Component {
+    render(){
+        return (
+            <h1>App</h1>
+        )
+    }
+}
+
+export default App
