7b5407f93a892522a403c10ff97e3f8fc956089c
william <willshinji@gmail.com>
2019-03-07 12:49:23 -0300

    create component footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default props => (
+    <div className='main-footer text-center'>
+        <strong>
+            Copyright 2019 - Develop by William &nbsp;
+            <a href='https://www.github.com/iwilliam317' target='_blank'>
+                <i className='fa fa-github'></i>
+            </a>
+        </strong>
+    </div>
+)
