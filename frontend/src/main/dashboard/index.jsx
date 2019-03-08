3f4f3af7b4904c176b284be0ae9e33823577d694
william <willshinji@gmail.com>
2019-03-08 16:13:05 -0300

    Update dashboard

--- frontend/src/main/dashboard/index.jsx
+++ frontend/src/main/dashboard/index.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <h1>Dashboard</h1>
-)
+import ContentHeader from '../../common/template/contentHeader'
+import Content from '../../common/template/content'
+
+class Dashboard extends Component {
+    render(){
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='version 1.0' />
+                <Content>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem perspiciatis, officia, odit a amet reprehenderit placeat expedita debitis ullam dolore reiciendis harum sapiente totam nostrum esse, magnam dolor animi cum.
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
