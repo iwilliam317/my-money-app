75d7c5905f607beb02ab0ab67cd1e91d480fb398
william <willshinji@gmail.com>
2019-03-08 21:26:18 -0300

    create grid component

--- frontend/src/main/dashboard/index.jsx
+++ frontend/src/main/dashboard/index.jsx
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../../common/template/contentHeader'
 import Content from '../../common/template/content'
+import Grid from '../../common/layout/grid'
 
 import api from '../../services/api'
 
 class Dashboard extends Component {
+    constructor(props){
+        super(props)
+        this.state = { credit: 0, debit: 0}
+    }
+
     getCredit(){
         api.get('/summary').then(res => {
             const { credit, debit} = res.data || 0
             console.log(credit, debit)
+            this.setState({ credit, debit})
         })
 
     }
@@ -22,7 +29,11 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='version 1.0' />
                 <Content>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem perspiciatis, officia, odit a amet reprehenderit placeat expedita debitis ullam dolore reiciendis harum sapiente totam nostrum esse, magnam dolor animi cum.
+                    <Grid cols='12 6 4 2'>
+                        <h2>{ this.state.credit }</h2>
+                        <h2>{ this.state.debit }</h2>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem perspiciatis, officia, odit a amet reprehenderit placeat expedita debitis ullam dolore reiciendis harum sapiente totam nostrum esse, magnam dolor animi cum.
+                    </Grid>
                 </Content>
             </div>
         )
