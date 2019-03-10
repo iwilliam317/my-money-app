ab700d9a2e6942f3950d9a9689bde8ffd1c324c1
william <willshinji@gmail.com>
2019-03-10 16:34:06 -0300

    Update index.jsx

--- frontend/src/main/dashboard/index.jsx
+++ frontend/src/main/dashboard/index.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../../common/template/contentHeader'
 import Content from '../../common/template/content'
-import Grid from '../../common/layout/grid'
 import ValueBox from '../../common/widget/valueBox'
 
 import api from '../../services/api'
@@ -15,7 +14,7 @@ class Dashboard extends Component {
 
     getSummary(){
         api.get('/summary').then(res => {
-            const { credit, debit} = res.data || 0
+            const { credit, debit } = res.data || 0
             this.setState({ credit, debit})
         })
 
@@ -30,9 +29,9 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='version 1.0' />
                 <Content>
-                    <ValueBox value={ this.state.debit } color='green' cols='4' text='Credit'/>
-                    <ValueBox value={ this.state.credit } color='red' cols='4' text='Credit'/>
-                    <ValueBox value={ this.state.debit - this.state.credit } color='blue' cols='4' text='Credit'/>
+                    <ValueBox value={ this.state.credit } color='green' cols='4' text='Credit'/>
+                    <ValueBox value={ this.state.debit } color='red' cols='4' text='Debit'/>
+                    <ValueBox value={ this.state.credit - this.state.debit } color='blue' cols='4' text='Remaining'/>
                    
                 </Content>
             </div>
