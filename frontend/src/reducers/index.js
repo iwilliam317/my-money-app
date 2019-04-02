3c4c94d5e5c599eb3174db6891fcf6b8eec170b2
william <willshinji@gmail.com>
2019-04-02 19:21:06 -0300

    adding form reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -3,6 +3,8 @@ import dashboard from './dashboard'
 import tab from './tab'
 import billingCycle from './billingCycle'
 
+import {reducer as formReducer} from 'redux-form'
+
 const reducers = combineReducers({
     dashboard,
     tab,
