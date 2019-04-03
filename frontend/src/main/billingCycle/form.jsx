4c5bc2631d8a216a009fd1e8dc2032fa38f8a155
william <willshinji@gmail.com>
2019-04-03 19:30:56 -0300

    first steps with redux-form

--- frontend/src/main/billingCycle/form.jsx
+++ frontend/src/main/billingCycle/form.jsx
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
+import {reduxForm, Field} from 'redux-form'
 
 class Form extends Component {
     render(){
         return (
-            <form role='form'>
-                <div className='box-body'></div>
+            <form role='form' onSubmit={this.props.handleSubmit}>
+                <div className='box-body'>
+                    <Field name='name' component='input'/>
+                    <Field name='month' component='input'/>
+                    <Field name='year' component='input'/>
+                </div>
                 <div className='box-footer'>
                     <button className="btn btn-primary" type='submit'>Submit</button>
                 </div>
@@ -13,4 +18,4 @@ class Form extends Component {
     }
 }
 
-export default Form
+export default reduxForm({form: 'Form'})(Form)
