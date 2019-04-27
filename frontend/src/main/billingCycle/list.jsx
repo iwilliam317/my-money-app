21bff7175dbf9b3386412fdb2196386c1734ab7c
william <willshinji@gmail.com>
2019-04-27 18:19:08 -0300

    add icon in button edit

--- frontend/src/main/billingCycle/list.jsx
+++ frontend/src/main/billingCycle/list.jsx
@@ -15,7 +15,7 @@ class List extends Component {
                     <td>{billing.name}</td>
                     <td>{billing.month}</td>
                     <td>{billing.year}</td>
-                    <td><button className='btn btn-warning' onClick={() => this.props.showBillingCycle(billing)}>edit</button></td>
+                    <td><button className='btn btn-warning' onClick={() => this.props.showBillingCycle(billing)}><i className='fa fa-edit'></i></button></td>
                 </tr>
             ))
         )
