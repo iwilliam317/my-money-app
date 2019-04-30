637a94d1c9d4a7cd2831fbd9449f670188ba605c
william <willshinji@gmail.com>
2019-04-30 20:40:57 -0300

    create credit list component *

---
+++ frontend/src/main/billingCycle/creditList.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {Field} from 'redux-form'
+
+class CreditList extends Component {
+    render(){
+        return (
+            <div className='col-lg-6 col-xs-12'>
+                <fieldset>
+                    <legend>Credits</legend>
+                    <Field name=''/>
+                    <Field name=''/>
+                </fieldset>
+            </div>
+        )
+    }
+}
+
+export default CreditList
