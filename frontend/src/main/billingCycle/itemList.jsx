7d0775d47e73ec62aef7bf57e89a751c5e64399f
william <willshinji@gmail.com>
2019-05-28 18:49:23 -0300

    summary - hard coded

--- frontend/src/main/billingCycle/itemList.jsx
+++ frontend/src/main/billingCycle/itemList.jsx
@@ -3,6 +3,9 @@ import {Field, arrayInsert, arrayRemove} from 'redux-form'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import Grid from '../../common/layout/grid'
+import Content from '../../common/template/content'
+import ValueBox from '../../common/widget/valueBox'
+import Row from '../../common/layout/row'
 
 class CreditList extends Component {
 
@@ -23,33 +26,43 @@ class CreditList extends Component {
         const list = this.props.list || []
         const {legend, field, cols} = this.props
         return (
-            <Grid cols={cols}>
-                <fieldset>
-                    <legend>{legend}</legend>
-                    <table className='table'>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Value</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {list.map((element, index) => (
-                                <tr key={index}>
-                                    <td><Field name={`${field}[${index}].name`} component='input'/></td>
-                                    <td><Field name={`${field}[${index}].value`} component='input'/></td>
-                                    <td>
-                                        <button type='button' className='btn btn-success' onClick={()=> this.add(index+1)}><i className="fa fa-plus"></i></button>
-                                        <button type='button' className='btn btn-warning' onClick={()=> this.add(index+1, element)}><i className='fa fa-clone'></i></button>
-                                        <button type='button' className="btn btn-danger" onClick={() => this.remove(index)}><i className=" fa fa-trash"></i></button>
-                                    </td>
+            <div>
+                <Content>
+                    <Row>
+                        <ValueBox value={ `R$ 500` } color='green' cols='12 4' text='Total Debit'  icon='credit-card'/>
+                        <ValueBox value={ `R$ 300` } color='red' cols='12 4' text='Total Credit' icon='bank' />
+                        <ValueBox value={ `R$ 200` } color='blue' cols='12 4' text='Balance' icon='money'/>
+                    </Row>
+               
+                </Content>
+                <Grid cols={cols}>
+                    <fieldset>
+                        <legend>{legend}</legend>
+                        <table className='table'>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Value</th>
+                                    <th>Actions</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </fieldset>
-            </Grid>
+                            </thead>
+                            <tbody>
+                                {list.map((element, index) => (
+                                    <tr key={index}>
+                                        <td><Field name={`${field}[${index}].name`} component='input'/></td>
+                                        <td><Field name={`${field}[${index}].value`} component='input'/></td>
+                                        <td>
+                                            <button type='button' className='btn btn-success' onClick={()=> this.add(index+1)}><i className="fa fa-plus"></i></button>
+                                            <button type='button' className='btn btn-warning' onClick={()=> this.add(index+1, element)}><i className='fa fa-clone'></i></button>
+                                            <button type='button' className="btn btn-danger" onClick={() => this.remove(index)}><i className=" fa fa-trash"></i></button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </fieldset>
+                </Grid>
+            </div>
         )
     }
 }
